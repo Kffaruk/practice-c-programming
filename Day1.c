624da1b3c31e8ff30127ef72d635177660d84e10
KF Faruk <kffaruk2020@gmail.com>
2025-05-15 01:37:41 +0600

    Practice C Data_Type

--- Day1.c
+++ Day1.c
@@ -3,36 +3,36 @@
 int main()
 {
 
-    /* int sum;
-    //int x = 20;
+    int sum;
+    int x = 20;
     float y = 10.5;
     sum = x + y;
 
-    printf("The value is:%.2f", y); */
+    printf("The value is:%.2f", y);
 
-    /*char math,bangla,eng;
-     math = 'A';
-     bangla = 'B';
-     eng = 'C';
-     printf("%c", eng);
+    char math, bangla, eng;
+    math = 'A';
+    bangla = 'B';
+    eng = 'C';
+    printf("%c", eng);
 
-     char ch;
-     ch = 65;
-     printf("%c\n", ch);
-     ch = 66;
-     printf("%c\n", ch);
-     ch = 67;
-     printf("%c\n", ch);
-     ch = 69;
-     printf("%c\n", ch);
-     ch = 70;
-     printf("%c\n", ch);
-     ch = 71;
-     printf("%c\n", ch);
-     ch = 72;
-     printf("%c\n", ch); */
+    char ch;
+    ch = 65;
+    printf("%c\n", ch);
+    ch = 66;
+    printf("%c\n", ch);
+    ch = 67;
+    printf("%c\n", ch);
+    ch = 69;
+    printf("%c\n", ch);
+    ch = 70;
+    printf("%c\n", ch);
+    ch = 71;
+    printf("%c\n", ch);
+    ch = 72;
+    printf("%c\n", ch);
 
-    /*float SSC_result;
+    float SSC_result;
     SSC_result = 4.50;
     printf("%.2f\n", SSC_result);
 
@@ -42,7 +42,7 @@ int main()
     float department = 55.5;
     float total_marks;
     total_marks = english + bangla + math + department;
-    printf("Total mark is:%.2f\n", total_marks); */
+    printf("Total mark is:%.2f\n", total_marks);
 
     return 0;
 }
