5a525d1da63630074a7eb85b32ea046e9b96edca
Omar Faruk <115606524+Kffaruk@users.noreply.github.com>
2025-07-10 22:39:07 +0600

    Update Day7.c

--- Day7.c
+++ Day7.c
@@ -98,5 +98,26 @@ int main()
     }
 
     
+    // problem 11
+
+    
+    
+    int x;
+    printf("Enter the number: ");
+    scanf("%d", &x);
+
+    if (x > 0)
+    {
+        printf("Positive\n");
+    }
+    else if (x < 0)
+    {
+        printf("Negative\n");
+    }
+    else
+    {
+        printf("Zero\n");
+    }
+    
     return 0;
 }
